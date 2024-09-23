a17a7e2f96037bc7885e15c2e7b6ae3998716971
Mayank Bansal <mayank.bansal.0527@gmail.com>
2024-09-23 23:23:46 +0530

    commit

--- solution.java
+++ solution.java
@@ -1,23 +1,46 @@
 import java.util.*;
-class solution{
-                
+class solution {
+
+    public static long fn(List<Integer> nums, int idx, int pre, long dp[][]) {
+        // base case
+        if(idx < 0) return 0;
+        if(idx==0) {
+            return (pre-idx) * nums.get(idx);
+        }
+
+        // not take 
+        if(dp[idx][pre] != -1) return dp[idx][pre];
+        long notTake = 0 + fn(nums, idx-1, pre, dp);
+
+        //take
+        long take = (pre-idx)*nums.get(idx) + fn(nums, idx-1, idx, dp);
+        return dp[idx][pre] = Math.max(notTake, take);
+        
+    }
+
+
+    public static long findMaximumScore(List<Integer> nums) {
+        int n = nums.size();
+        long dp[][] = new long[n][n];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+
+        return fn(nums, n-1, n-1, dp);
+    }
+
 
 public static void main(String[] args) {
-    // Scanner sc = new Scanner(System.in);
-    // int t = sc.nextInt();
-    // while(t-->0) {
-    //     int n = sc.nextInt();
-    //     int cows = n/4;
-    //     int hens = n%4;
-
-    //     System.out.println(cows + hens);
-    // }
-    // System.out.println(7%8);
-    int arr[] = {22, 16, 36, 18, 4, 21, 28 ,2 ,7, 35, 9 ,15, 5, 23 ,10 ,20};
-    Arrays.sort(arr);
-    for(int i=0; i<arr.length; i++) {
-        System.out.print(arr[i] + " ");;
-    }
+    List<Integer> list = new ArrayList<>();
+    list.add(4);
+    list.add(3);
+    list.add(1);
+    list.add(3);
+    list.add(2);
+    long ans = findMaximumScore(list);
+    System.out.println(ans);
+    
 }
              
 }
